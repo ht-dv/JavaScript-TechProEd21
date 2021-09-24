94840e1310ac1a007d32081f2dcc6c831bbc3c37
HT <ht.thedev@gmail.com>
2021-09-25 01:55:42 +0300

    V1

---
+++ 04 DOM/Ornek01_Selector/app.js
@@ -0,0 +1,6 @@
+/*========= getElementById() ==========*/
+
+const paragraf = document.getElementById("par").style;
+paragraf.backgroundColor = "black";
+paragraf.color = "white";
+paragraf.fontSize = "20px";
