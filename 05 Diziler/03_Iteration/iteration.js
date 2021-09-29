e727d7afa321c5bf47e4e3d10e98fd2fa7bf0b7e
HT <ht.thedev@gmail.com>
2021-09-30 00:03:40 +0300

    29.09

---
+++ 05 Diziler/03_Iteration/iteration.js
@@ -0,0 +1,15 @@
+//**==============DIZILERDE ITERATION============== */
+
+//*====FOR DONGUSU====*/
+
+const koordinatlar = [120, 100, -100, 220, 330, -50];
+
+let pozitif = 0;
+let negatif = 0;
+for (let i = 0; i < koordinatlar.length; i++) {
+  koordinatlar[i] < 0
+    ? (negatif = negatif + koordinatlar[i])
+    : (pozitif = pozitif + koordinatlar[i]);
+}
+
+console.log(`Negatifler toplami ${negatif}, pozitifler toplami ${pozitif}`);
